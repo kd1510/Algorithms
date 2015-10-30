88d488e033d3ccab642bd2274a45be301fe83217
Krishan Davda <krishan@Krishans-MacBook-Air.local>
2015-10-30 11:32:21 +0000

    updated and fixed quicksort

--- sorting/quicksort.py
+++ sorting/quicksort.py
@@ -3,42 +3,39 @@ import random
 import pdb
 
 def partition_array(array, low, high):
+
 	'''
 	This needs to partition the array in O(n) time.
 	Partitioning the array consists of moving the elements less than the pivot
 	to the left side of the elements that are greater than the pivot.
+
+	This version of partition always takes the leftmost element of the subarray as the pivot.
+	This gives O(n^2) running time when the order of the list is reversed.
 	'''
 
 	if len(array[low:high]) < 2:
 		return low
 
-	pivot_index = random.randrange(low,high)	
-	pivot = array[pivot_index]
-
-	#Swap the pivot with the first element of the array if it isn't
-	if pivot_index != low:
-		low_val = array[low]
-		array[pivot_index] = low_val
-		array[low] = pivot
+	pivot = array[low]
 
 	i = low 
 	j = low + 1
 
-	while j < len(array[low:high]):
-		
-		#If the value is greater than the pivot, then do nothing (encorporates into sorted >p).
+	while j < high:
+	
+		#If the value is greater than the pivot, then do nothing 
+		#as encorporates into sorted >p.
 		if array[j] > pivot:
 			pass 
 
-		#If value is less than the pivot, then swap it with the i+1 element (this is going to move the boundary i to i+1)
+		#If value is less than the pivot, then swap it with the i+1 element
+		# as this is going to move the boundary i to i+1.
 		elif array[j] < pivot:
 			i_plus_one_val = array[i+1]
 			j_val = array[j]
-
 			array[i+1] = j_val
 			array[j] = i_plus_one_val
 			i += 1
-
 		j += 1
 
 	#Swap the pivot on the end with the value at i to get it in the right place.
@@ -46,21 +43,19 @@ def partition_array(array, low, high):
 	array[low] = i_val
 	array[i] = pivot
 
-	return i
+	return i 
 
-def quicksort(array, low, high):
+def quicksort(array, low=None, high=None):
 	
-	if low != high:
-		pivot = partition_array(array, low, high)
-		quicksort(array, low, pivot)
-		quicksort(array, pivot, high)
-	
-	elif low == high:
-		return None
+	if high == None and low == None:
+		low = 0
+		high = len(array)
 
-array = [8,7,6,5,4,3,2,1,0]
+	if low < high:
+		current_pivot = partition_array(array, low, high)
+		quicksort(array, low, current_pivot)
+		quicksort(array, current_pivot+1, high)
 
-quicksort(array, 0, len(array))
-
-
-
+array = [5,4,1,7,9,2,6]
+quicksort(array)
+print array
---
+++ sorting/randomised_quicksort.py
@@ -0,0 +1,11 @@
+
+#This subroutine is used if we randomise the pivot.
+# pivot_index = random.randrange(low,high)	
+# pivot = array[pivot_index]
+
+# if pivot_index != low:
+# 	low_val = array[low]
+# 	array[pivot_index] = low_val
+# 	array[low] = pivot
+
+
